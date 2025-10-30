9b7a7ed648f72878f1eb3ab8d061d422fef288da
Itto Hase <abannofabiao@gmail.com>
2025-10-30 13:15:22 +0900

    Support integer calculation

--- test.bash
+++ test.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
-#
+
 ng () {
         echo ${1}行目が違うよ
         res=1
 }
+
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
+out=$(seq 5 | ./plus)
+test "${out}" = 15.0 || ng "$LINENO"
+
+test "${res}" = 0 && echo OK
 exit $res
