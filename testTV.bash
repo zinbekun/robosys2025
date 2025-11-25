d851f965c9fd3bda87815520cebae9e17571a3bb
Itto Hase <abannofabiao@gmail.com>
2025-11-25 18:04:00 +0900

    testTV再修正

--- testTV.bash
+++ testTV.bash
@@ -8,25 +8,25 @@ ng () {
 res=0
 
 # ====== 1: ドラえもん ======
-out=$(printf "1\n" | python3 program.py)
+out=$(printf "1\n" | python3 TVtimer.py)
 echo "$out" | grep "ドラえもん" >/dev/null || ng "$LINENO"
 echo "$out" | grep "次のドラえもんの放送開始まで" >/dev/null || ng "$LINENO"
 
 
 # ====== 2: クレヨンしんちゃん ======
-out=$(printf "2\n" | python3 program.py)
+out=$(printf "2\n" | python3 TVtimer.py)
 echo "$out" | grep "クレヨンしんちゃん" >/dev/null || ng "$LINENO"
 echo "$out" | grep "次のクレヨンしんちゃんの放送開始まで" >/dev/null || ng "$LINENO"
 
 
 # ====== 3: サザエさん ======
-out=$(printf "3\n" | python3 program.py)
+out=$(printf "3\n" | python3 TVtimer.py)
 echo "$out" | grep "サザエさん" >/dev/null || ng "$LINENO"
 echo "$out" | grep "次のサザエさんの放送まで開始" >/dev/null || ng "$LINENO"
 
 
 # ====== 4: ちびまる子ちゃん ======
-out=$(printf "4\n" | python3 program.py)
+out=$(printf "4\n" | python3 TVtimer.py)
 echo "$out" | grep "ちびまる子ちゃん" >/dev/null || ng "$LINENO"
 echo "$out" | grep "次のちびまる子ちゃんの放送開始まで" >/dev/null || ng "$LINENO"
 
@@ -39,7 +39,7 @@ echo "$out" | grep "次のちびまる子ちゃんの放送開始まで" >/dev/n
 # 21          時
 # 15          分
 # 7           周期
-out=$(printf "5\nTestTV\n5\n21\n15\n7\n" | python3 program.py)
+out=$(printf "5\nTestTV\n5\n21\n15\n7\n" | python3 TVtimer.py)
 echo "$out" | grep "TestTV" >/dev/null || ng "$LINENO"
 echo "$out" | grep "次のTestTVの放送開始まで" >/dev/null || ng "$LINENO"
 
