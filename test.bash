c4916f0f34996508aa799cc1013ff0f8041ffbd6
Itto Hase <abannofabiao@gmail.com>
2025-12-10 12:40:58 +0900

    test独立コマンド使用

--- test.bash
+++ test.bash
@@ -10,68 +10,68 @@ ng () {
 res=0
 
 # 1 ドラえもん
-out=$(python3 ./tvtimer 1)
+out=$(./tvtimer 1)
 echo "$out" | grep "days" >/dev/null || ng "$LINENO"
 
 # 2 クレヨンしんちゃん
-out=$(python3 ./tvtimer 2)
+out=$(./tvtimer 2)
 echo "$out" | grep "days" >/dev/null || ng "$LINENO"
 
 # 3 サザエさん
-out=$(python3 ./tvtimer 3)
+out=$(./tvtimer 3)
 echo "$out" | grep "days" >/dev/null || ng "$LINENO"
 
 # 4 ちびまる子ちゃん
-out=$(python3 ./tvtimer 4)
+out=$(./tvtimer 4)
 echo "$out" | grep "days" >/dev/null || ng "$LINENO"
 
 # 5 その他（任意の曜日・時刻・周期）
 # 引数: 5 曜日 時 分 周期
 # 例: 月曜日=0, 15:21, 7日周期
-out=$(python3 ./tvtimer 5 0 15 21 7)
+out=$(./tvtimer 5 0 15 21 7)
 echo "$out" | grep "days" >/dev/null || ng "$LINENO"
 
 #errror
-out=$(python3 ./tvtimer 6)
+out=$(./tvtimer 6)
 echo "$out" | grep "ERROR:Specify a number (1–5)" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 2 5)
+out=$(./tvtimer 2 5)
 echo "$out" | grep "ERROR:Only one entry is allowed" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer a)
+out=$(./tvtimer a)
 echo "$out" | grep "ERROR:Enter the number using digits" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer @)
+out=$(./tvtimer @)
 echo "$out" | grep "ERROR:Enter the number using digits" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer )
+out=$(./tvtimer )
 echo "$out" | grep -F "ERROR:Select a TVprogram [1-5]" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 8 20 20 7)
+out=$(./tvtimer 5 8 20 20 7)
 echo "$out" | grep "ERROR:Specify the day of the week (0 = Monday, 6 = Sunday)" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 5 30 20 7)
+out=$(./tvtimer 5 5 30 20 7)
 echo "$out" | grep "ERROR:Specify the hour (0–23)" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 5 20 80 7)
+out=$(./tvtimer 5 5 20 80 7)
 echo "$out" | grep "ERROR:Specify the minutes (0–59)" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 5 30 20)
+out=$(./tvtimer 5 5 30 20)
 echo "$out" | grep "ERROR:Please enter only the correct information for weekday, hour, minute, and interval" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 5 30 20 7 9)
+out=$(./tvtimer 5 5 30 20 7 9)
 echo "$out" | grep "ERROR:Please enter only the correct information for weekday, hour, minute, and interval" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 a 20 30 7)
+out=$(./tvtimer 5 a 20 30 7)
 echo "$out" | grep "ERROR:Enter the number using digits" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 5 @ 30 7)
+out=$(./tvtimer 5 5 @ 30 7)
 echo "$out" | grep "ERROR:Enter the number using digits" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 5 20 b 7)
+out=$(./tvtimer 5 5 20 b 7)
 echo "$out" | grep "ERROR:Enter the number using digits" >/dev/null || ng "$LINENO"
 
-out=$(python3 ./tvtimer 5 5 20 80 z)
+out=$(./tvtimer 5 5 20 80 z)
 echo "$out" | grep "ERROR:Enter the number using digits" >/dev/null || ng "$LINENO"
 
 test "${res}" = 0 && echo OK
